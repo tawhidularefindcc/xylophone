254d5c0d21e45fe5d1246080be5e5237e95b6c18
Tawhidul Arefin <tawhidularefin530@gmail.com>
2022-08-22 16:05:25 +0600

    Cmpleted: xyloPhone App

--- xylophone/xylophone/ViewController.swift
+++ xylophone/xylophone/ViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +18,20 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(soundName: String){
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else { return }
+        player = try! AVAudioPlayer(contentsOf: url)
+        player.play()
+    }
 }
 
